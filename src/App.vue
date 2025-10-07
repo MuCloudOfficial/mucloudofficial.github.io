48261c601a3629a6c1e5da0610853546ecf57155
Mu_Cloud <Mu_Cloud@outlook.de>
2025-10-07 09:40:01 +0800

    [DEV.7] Update Vite to 7.1.5 | Adjust Buttons

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import Navigation from './components/Navigation.vue'
-import test from './components/test.vue'
 </script>
 
 <template>
@@ -10,9 +9,9 @@ import test from './components/test.vue'
     <h1 class="mx-auto text-center my-5 text-gray-300 text-3xl font-bold">Mu_Cloud</h1>
     <p class="mx-auto text-center text-gray-300 text-base">MuCloud_Official</p>
     <div class="mx-auto w-full flex flex-row items-center justify-center p-5">
-        <button class="rounded-full w-40 h-10 mx-2 bg-gray-500 transition delay-10 duration-300 ease-in-out hover:bg-gray-400"><a href="https://mucloudofficial.github.io/MuBlog">MuBlog</a></button>
-        <button class="rounded-full w-40 h-10 mx-2 bg-gray-500 transition delay-10 duration-300 ease-in-out hover:bg-gray-400"><a href="https://mucloudofficial.github.io/MuCloudProjectDoc">MuProjectDoc</a></button>
-        <button class="rounded-full w-40 h-10 mx-2 bg-gray-500 transition delay-10 duration-300 ease-in-out hover:bg-gray-400"><a href="https://github.com/MuCloudOfficial">GithubPage</a></button>
+        <button @click.capture="open('https://mucloudofficial.github.io/MuBlog', '_blank')" class="rounded-full w-40 h-10 mx-2 bg-gray-500 transition delay-10 duration-300 ease-in-out hover:bg-gray-400">MuBlog</button>
+        <button @click.capture="open('https://mucloudofficial.github.io/MuCloudProjectDoc', '_blank')" class="rounded-full w-40 h-10 mx-2 bg-gray-500 transition delay-10 duration-300 ease-in-out hover:bg-gray-400">MuProjectDoc</button>
+        <button @click.capture="open('https://github.com/MuCloudOfficial', '_blank')" class="rounded-full w-40 h-10 mx-2 bg-gray-500 transition delay-10 duration-300 ease-in-out hover:bg-gray-400">GithubPage</button>
     </div>
     <footer class="w-full absolute bottom-0">
         <p class="mx-auto text-center p-2 text-gray-300 text-sm">Design by Mu_Cloud | MADE IN VOID LAND | Based on HTML & VUE & TailwindCSS | <a class="underline" href="https://github.com/MuCloudOfficial/mucloudofficial.github.io">Page Source Code</a></p>
