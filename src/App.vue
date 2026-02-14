9092970a9cf37710f7ada480dca4044befa21497
Mu_Cloud <Mu_Cloud@outlook.de>
2026-02-14 10:44:27 +0800

    Update Project Links

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,8 @@ import Navigation from './components/Navigation.vue'
     <h1 class="mx-auto text-center my-5 text-gray-300 text-3xl font-bold">Mu_Cloud</h1>
     <p class="mx-auto text-center text-gray-300 text-base">MuCloud_Official</p>
     <div class="mx-auto w-full flex flex-row items-center justify-center p-5">
-        <a href="https://mucloudofficial.github.io/MuBlog" target="_blank"><button class="buttonBase">MuBlog</button></a>
-        <a href="https://mucloudofficial.github.io/MuCloudProjectDoc" target="_blank"><button class="buttonBase">MuProjectDoc</button></a>
+        <a href="https://blog.muverse.ltd" target="_blank"><button class="buttonBase">MuBlog</button></a>
+        <a href="https://doc.muverse.ltd" target="_blank"><button class="buttonBase">MuProjectDoc</button></a>
         <a href="https://github.com/MuCloudOfficial" target="_blank"><button class="buttonBase">GithubPage</button></a>
     </div>
     <footer class="w-full absolute bottom-0">
