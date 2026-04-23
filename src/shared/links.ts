c7f427d2e1dad2f0da81be36ca2a2a57fca7f360
Mu_Cloud <Mu_Cloud@outlook.de>
2026-04-23 13:05:44 +0800

    Update links.ts

--- src/shared/links.ts
+++ src/shared/links.ts
@@ -1,4 +1,4 @@
 export const MUBLOG = "https://blog.muverse.ltd";
 export const MUPROJECTDOC = "https://doc.muverse.ltd";
-export const MUGITHUB = "https://github.com/MuCloudOffical";
+export const MUGITHUB = "https://github.com/MuCloudOfficial";
 export const REPO = "https://github.com/MuCloudOfficial/mucloudofficial.github.io";
